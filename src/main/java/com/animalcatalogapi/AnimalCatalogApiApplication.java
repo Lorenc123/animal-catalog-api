549331fc0bd863616a9c6c51a6db6639f73f3803
Lorenc123 <lorencqorri66@gmail.com>
2024-05-19 09:24:40 +0200

    first comit

---
+++ src/main/java/com/animalcatalogapi/AnimalCatalogApiApplication.java
@@ -0,0 +1,13 @@
+package com.animalcatalogapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalCatalogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalCatalogApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/animalcatalogapi/controllers/AdviceController.java
@@ -0,0 +1,4 @@
+package com.animalcatalogapi.controllers;
+
+public class AdviceController {
+}
